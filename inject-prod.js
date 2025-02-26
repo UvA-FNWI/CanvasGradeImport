97bdaf638b545d85ba7f997819d85a3b9a2cf657
Gerrit Oomens <g.oomens@uva.nl>
2025-02-26 10:36:56 +0100

    Add another gradebook import url (?)

--- inject-prod.js
+++ inject-prod.js
@@ -1,4 +1,4 @@
-if (window.location.pathname.indexOf('/gradebook_upload/new') !== -1) {
+if (window.location.pathname.indexOf('/gradebook_upload/new') !== -1 || window.location.pathname.endsWith('/gradebook_uploads')) {
     const s = document.createElement("script");
     s.type = "text/javascript";
     s.src = "https://canvastools.datanose.nl/import.js";
